8a187394a804e66d97027cbaa90539f6ea3f98b5
Shabinn-k <shhabin.07@gmail.com>
2026-01-07 10:50:40 +0530

    update

--- redux-1/src/components/Todo.jsx
+++ redux-1/src/components/Todo.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
-import { useSelector,useDispatch } from 'react-redux'
-import { addTodo,deleteTodo,editTodo } from '../redux/todoSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { addTodo, deleteTodo, editTodo } from '../redux/todoSlice'
+
 const Todo = () => {
-  const dispatch=useDispatch()
-  const items=useSelector(state=>state.todo.items)
-  const [text,setText]=useState("")
-  const [editId,setEditId]=useState(null)
-  const addOrEdit=()=>{
-    if(!text.trim())return 
-    if(editId){
-      dispatch(editTodo({id:editId,newText:text}))
+  const dispatch = useDispatch()
+  const items = useSelector(state => state.todo.items)
+  const [text, setText] = useState("")
+  const [editId, setEditId] = useState(null)
+  const addOrEdit = () => {
+    if (!text.trim()) return
+    if (editId) {
+      dispatch(editTodo({ id: editId, newText: text }))
       setEditId(null)
-    }else{
+    } else {
       dispatch(addTodo(text))
     }
     setText("")
@@ -19,19 +20,19 @@ const Todo = () => {
   return (
     <div>
       <h1>TO DO APP</h1>
-      <input type="text" value={text} onChange={(e)=>setText(e.target.value)}
-      placeholder='Enter...' />
-      <button onClick={addOrEdit}>{editId? "Update":"Add"}</button>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)}
+        placeholder='Enter...' />
+      <button onClick={addOrEdit}>{editId ? "Update" : "Add"}</button>
 
-    <ul>
-      {items.map(todo=>(
-        <li key={todo.id}>
-          <span>{todo.text}</span>
-          <button onClick={()=>{setEditId(todo.id);setText(todo.text)}}>Edit</button>
-          <button onClick={()=>dispatch(deleteTodo(todo.id))}>Remove</button>
-        </li>
-      ))}
-    </ul>
+      <ul>
+        {items.map(todo => (
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={() => { setEditId(todo.id); setText(todo.text) }}>Edit</button>
+            <button onClick={() => dispatch(deleteTodo(todo.id))}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
